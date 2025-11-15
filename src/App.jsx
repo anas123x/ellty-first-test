bf5ba174765b1b062697cd1b9983c75a854b5b2b
Anas Dai <anas.dai@esprit.tn>
2025-11-15 13:06:10 +0100

    first commit : card + custom font

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center pt-[85px] ">
+      <div
+        className="w-[370px] h-[326px] rounded-md border border-[#EEEEEE] py-2.5 "
+        style={{
+          boxShadow: "0px 8px 15px 0px #1414141F, 0px 0px 4px 0px #1414141A",
+        }}
+      >
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
