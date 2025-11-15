014e00710bf9cb9ee59669927a2977d08a38559e
Anas Dai <anas.dai@esprit.tn>
2025-11-15 13:16:39 +0100

    Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
     <div className="flex justify-center items-center pt-[85px] ">
       <div
-        className="w-[370px] h-[326px] rounded-md border border-[#EEEEEE] py-2.5 "
+        className="w-[370px] h-[326px] rounded-md border border-[#EEEEEE] py-2.5  flex flex-col justify-between items-center "
         style={{
           boxShadow: "0px 8px 15px 0px #1414141F, 0px 0px 4px 0px #1414141A",
         }}
       >
-       
+       <Button />
       </div>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export const Button = ({ children, onClick, className }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  return (
+    <button className="bg-[#FFCE22] w-[340px] h-[40px] rounded-sm font-montserrat text-[14px] py-2.5 px-5 hover:bg-[#FFD84D] hover:cursor-pointer active:bg-[#FFCE22] active:cursor-default">
+      Done
+    </button>
+  );
+};
